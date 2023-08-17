a3c499b85b504904d8821404ec885bf72fd7ddec
nyantama0616 <r_nyantama0616@yahoo.co.jp>
2023-08-17 22:04:07 +0900

    reactのルーティング設定

--- front-end/src/components/App.tsx
+++ front-end/src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopPage from './pages/TopPage';
+import SignUpPage from './pages/SignUpPage';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <TopPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<TopPage/>}></Route>
+          <Route path="/sign-up" element={<SignUpPage/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
